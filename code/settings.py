924b6e85cfb93c267796d6077be23934d6071485
RiverBlakkk <kamil.atlasiewcz@gmail.com>
2022-04-02 21:05:59 +0200

    lll

---
+++ code/settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                                                        ',
+'                                                        ',
+'                                                        ',
+' XX    XXX            XX                                ',
+' XX                                                     ',
+' XXXX         XX         XX                             ',
+'XXXXXXXXXXXXXX                                          ',
+'X            XXXX     X  X                              ',
+'X                   XX  XX                              ',
+'X  P              XX   XX                               ',
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+
+tile_size = 32
+screen_width = 800
+screen_height = len(level_map) * tile_size
--- code/tiles.py
+++ code/tiles.py
@@ -1,12 +1,12 @@
-import pygame 
+import pygame
 
-color1 = (200, 168, 90)
+color = (200, 168, 90)
 
 class Tile(pygame.sprite.Sprite):
 	def __init__(self,pos,size):
 		super().__init__()
 		self.image = pygame.Surface((size,size))
-		self.image.fill(color1)
+		self.image.fill(color)
 		self.rect = self.image.get_rect(topleft = pos)
 
 	def update(self,x_shift):
